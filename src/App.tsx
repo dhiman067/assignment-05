9b0dbf754b2795e015a69f915401cf0727dd8e65
dhiman067 <dhimanpaul119@gmail.com>
2026-09-11 01:33:55 +0600

    cards section done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Suspense } from 'react'
 import './App.css'
 import Banner from './Components/Banner-Section/Banner'
+import Navbar from './Components/Banner-Section/Navbar'
 import TechCards from './Components/TechCards/TechCards'
 import type { Itech } from './Type'
 
@@ -17,7 +18,8 @@ function App() {
 
   return (
     <>
-     <Banner></Banner>
+      <Navbar></Navbar>
+      <Banner></Banner>
      <Suspense fallback={<h1>Loadingg....</h1>}>
                 <TechCards TechCardPromise={TechCardPromise()}></TechCards>
      </Suspense>
--- src/Components/TechCards/TechCards.tsx
+++ src/Components/TechCards/TechCards.tsx
@@ -9,12 +9,14 @@ export interface TechCardsProps {
 export default function TechCards({ TechCardPromise }: TechCardsProps) {
     let techCards = use(TechCardPromise)
     return (
-       <div className="w-[1500px] border-2 mx-auto">
-        <h1>Explore the <span>Technologies</span></h1>
-        <p>Pick one technology per category to build your ideal stack.</p>
+       <div className="xl:w-[1500px] mx-auto">
+        <div className="flex flex-col items-center xl:items-start">
+            <h1 className="text-3xl font-bold">Explore The <span className="text-pink-500">Technologies</span></h1>
+        <p className="text-[#64748B]">Pick one technology per category to build your ideal stack.</p>
 
+        </div>
         {/* Cards*/}
-        <div className="xl:grid xl:grid-cols-3 gap-4 py-4">
+        <div className="xl:grid xl:grid-cols-3 gap-4 py-4 flex flex-col items-center">
             {
                 techCards.map(card => <TechCard key={card.id} card={card}></TechCard>)
             }
