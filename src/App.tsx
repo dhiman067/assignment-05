b9b4290c609ae813789b2eeacb16e7d487d6101b
dhiman067 <dhimanpaul119@gmail.com>
2026-09-10 23:23:02 +0600

    navbar created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Banner from './Components/Banner-Section/Banner'
+
+function App() {
+  
+
+  return (
+    <>
+     <Banner></Banner>
+     
+
+    </>
+  )
+}
+
+export default App
